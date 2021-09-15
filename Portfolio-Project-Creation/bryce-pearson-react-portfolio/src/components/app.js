68e0268e4e4adb93309d76cfed0ec1b82c948678
BryceKrispy9 <brycepearson09@gmail.com>
2021-09-14 20:31:20 -0600

    First Portfolio commit

---
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Bryce Pearson Portfolio</h1>
+      </div>
+    );
+  }
+}
