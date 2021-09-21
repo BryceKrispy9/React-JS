6787ebf7b158543ec3d3e14a6bd84386f81563e0
BryceKrispy9 <brycepearson09@gmail.com>
2021-09-20 19:22:27 -0600

    this.state

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-container.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-container.js
@@ -6,21 +6,28 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
+        this.state = {
+            pageTitle: "Welcome to my Portfolio!",
+            data: [
+                { title: "Xima Software" },
+                { title: "Eventbrite" },
+                { title: "Ministry Safe" }
+            ]
+        };
+
         console.log("Portfolio container has rendered");
     }
 
     portfolioItems() {
-        const data = ["Xima Software", "Eventbrite", "Ministry Safe"];
-
-        return data.map(item => {
-            return <PortfolioItem title={item} url={"google.com"} />;
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />;
         });
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio items go here...</h2>
+                <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
             </div>
