53d285c915cfbfba7564e981bfbbaba0ce8e010f
BryceKrispy9 <brycepearson09@gmail.com>
2021-09-26 21:27:06 -0600

    Ternary Operators pt 2

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/navigation/navigation-container.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/navigation/navigation-container.js
@@ -12,7 +12,7 @@ export default class NavigationContainer extends Component {
                 <button>About</button>
                 <button>Contact</button>
                 <button>Blog</button>
-                {false ? <button>Add Blog</button> : null }
+                {false ? <button>Add Blog</button> : null}
             </div>
         )
     }
