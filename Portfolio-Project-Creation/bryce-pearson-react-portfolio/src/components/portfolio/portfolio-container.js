8c02000f7dbe0cb38ca07dcdaf1377cbb72db275
BryceKrispy9 <brycepearson09@gmail.com>
2021-09-29 20:48:01 -0600

    Noting debugger - react browser dev tools

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-container.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-container.js
@@ -44,6 +44,7 @@ export default class PortfolioContainer extends Component {
 
     portfolioItems() {
         return this.state.data.map(item => {
+            // debugger; // item, item.banner_image_url, item.name, item.description (Freezes execution of program)
             console.log("portfolio item", item);
             return <PortfolioItem key = {item.id} title={item.name} url={item.url} slug = {item.id} />;
         });
