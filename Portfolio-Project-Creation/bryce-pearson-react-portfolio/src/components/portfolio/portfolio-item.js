43cbe48ba8466928be89283bb31ee337f0d6e4ab
BryceKrispy9 <brycepearson09@gmail.com>
2021-10-03 21:27:15 -0600

    Update styles with react listeners

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-item.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-item.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function(props) {
-    // Data that we'll need
-    // - background image: thumb_image_url
-    // - logo: logo_url?
-    // - description: description
-    // - id: id
+export default class PortfolioItem extends Component {
+    constructor(props) {
+        super(props);
 
-    const { id, description, thumb_image_url, logo_url } = props.item;
+        this.state = {
+            PortfolioItemClass: ""
+        };
+    }
+
+    handleMouseEnter() {
+        this.setState({PortfolioItemClass: 'image-blur'});
+    }
+
+    handleMouseLeave() {
+        this.setState({PortfolioItemClass: ""});
+    }
+
+    render() {
+    const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
-        <div className = "portfolio-item-wrapper">
+        <div className = "portfolio-item-wrapper"
+        onMouseEnter = {() => this.handleMouseEnter()}
+        onMouseLeave = {() => this.handleMouseLeave()}
+        
+        >
         <div
-            className = "portfolio-img-background"
+            className = {"portfolio-img-background " + this.state.PortfolioItemClass}
             style = {{
                 backgroundImage: "url(" + thumb_image_url + ")"
             }}
@@ -26,5 +41,6 @@ export default function(props) {
             <div className = "subtitle">{description}</div>
         </div>
         </div>
-    );
+        );
+    }
 }
