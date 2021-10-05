a36fe76ab347d1c72787cecf8518bb5204d47a4f
BryceKrispy9 <brycepearson09@gmail.com>
2021-10-04 21:19:39 -0600

    Adding auth image

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/pages/auth.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/pages/auth.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import loginImage from "../../../static/assets/images/auth/login.jpg";
 
 export default class Auth extends Component {
     render() {
         return (
-            <div>
-                Auth...
+            <div className = "auth-page-wrapper">
+                <div 
+                className = "left-side"
+                style = {{
+                    backgroundImage: `url(${loginImage})`
+                }}
+                />
+
+                <div className = "right-side">
+                    <h1>Login component goes here...</h1>
+                </div>
             </div>
         );
     }
