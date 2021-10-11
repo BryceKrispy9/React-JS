07e52b5070865d4f4c656ef84212b25c6c1fba98
BryceKrispy9 <brycepearson09@gmail.com>
2021-10-10 20:17:04 -0600

    Showing and Hiding links when logged in

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/navigation/navigation-container.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/navigation/navigation-container.js
@@ -2,6 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavigationContainer = (props) => {
+    const dynamicLink = (route, linkText) => {
+        return (
+            <div className = "nav-link-wrapper">
+                <NavLink to = "/blog" activeClassName = "nav-link-active">Blog</NavLink>
+            </div>
+        );
+    };
         return (
             <div className = "nav-wrapper">
                 <div className = "left-column">
@@ -17,9 +24,7 @@ const NavigationContainer = (props) => {
             <NavLink to = "/contact" activeClassName = "nav-link-active">Contact</NavLink>
                     </div>
 
-                    <div className = "nav-link-wrapper">
-            <NavLink to = "/blog" activeClassName = "nav-link-active">Blog</NavLink>
-                    </div>
+                    {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
                 </div>
 
                 <div className = "right-column">Bryce Pearson</div>
