5d60593ba62df8242fc181996f367977ec428e4b
BryceKrispy9 <brycepearson09@gmail.com>
2021-10-10 20:19:11 -0600

    Adding Prettier format extension

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/app.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/app.js
@@ -1,108 +1,118 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
-import PortfolioContainer from './portfolio/portfolio-container';
-import NavigationContainer from './navigation/navigation-container';
-import Home from './pages/home';
-import About from './pages/about';
-import Contact from './pages/contact';
-import Blog from './pages/blog';
-import portfolioDetail from './portfolio/portfolio-detail';
-import Auth from './pages/auth';
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import portfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      loggedInStatus: "NOT_LOGGED_IN"
-    };
+		this.state = {
+			loggedInStatus: "NOT_LOGGED_IN",
+		};
 
-    this.handleSuccesfulLogin = this.handleSuccesfulLogin.bind(this);
-    this.handleUnsuccesfulLogin = this.handleUnsuccesfulLogin.bind(this); 
-  }
+		this.handleSuccesfulLogin = this.handleSuccesfulLogin.bind(this);
+		this.handleUnsuccesfulLogin = this.handleUnsuccesfulLogin.bind(this);
+	}
 
-  handleSuccesfulLogin() {
-    this.setState({
-      loggedInStatus: "LOGGED_IN"
-    })
-  }
+	handleSuccesfulLogin() {
+		this.setState({
+			loggedInStatus: "LOGGED_IN",
+		});
+	}
 
-  handleUnsuccesfulLogin() {
-    this.setState({
-      loggedInStatus: "NOT_LOGGED_IN"
-    })
-  }
+	handleUnsuccesfulLogin() {
+		this.setState({
+			loggedInStatus: "NOT_LOGGED_IN",
+		});
+	}
 
-  checkLoginStatus() {
-    return axios.get("https://api.devcamp.space/logged_in", { 
-      withCredentials: true 
-    }).then(response => {
-      const loggedIn = response.data.logged_in;
-      const loggedInStatus = this.state.loggedInStatus;
+	checkLoginStatus() {
+		return axios
+			.get("https://api.devcamp.space/logged_in", {
+				withCredentials: true,
+			})
+			.then((response) => {
+				const loggedIn = response.data.logged_in;
+				const loggedInStatus = this.state.loggedInStatus;
 
-      // If loggedIn and status LOGGED_IN => return data
-      // If loggedIn and status NOT_LOGGED_IN => update state
-      // If not loggedIn and status LOGGED_IN => update state
+				// If loggedIn and status LOGGED_IN => return data
+				// If loggedIn and status NOT_LOGGED_IN => update state
+				// If not loggedIn and status LOGGED_IN => update state
 
-      if (loggedIn && loggedInStatus === "LOGGED_IN") {
-        return loggedIn;
-      } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
-        this.setState({
-          loggedInStatus: "LOGGED_IN"
-        });
-      } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
-        this.setState({
-          loggedInStatus: "NOT_LOGGED_IN"
-        });
-      }
-    })
-    .catch(error => {
-      console.log("Error", error);
-    })
-  }
+				if (loggedIn && loggedInStatus === "LOGGED_IN") {
+					return loggedIn;
+				} else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+					this.setState({
+						loggedInStatus: "LOGGED_IN",
+					});
+				} else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+					this.setState({
+						loggedInStatus: "NOT_LOGGED_IN",
+					});
+				}
+			})
+			.catch((error) => {
+				console.log("Error", error);
+			});
+	}
 
-  componentDidMount() {
-    this.checkLoginStatus();
-  }
+	componentDidMount() {
+		this.checkLoginStatus();
+	}
 
-  render() {
-    return (
-      <div className= 'container'>
-        <Router>
-          <div>
-          <NavigationContainer loggedInStatus = {this.state.loggedInStatus} />
+	render() {
+		return (
+			<div className="container">
+				<Router>
+					<div>
+						<NavigationContainer
+							loggedInStatus={this.state.loggedInStatus}
+						/>
 
-          <h2>{this.state.loggedInStatus}</h2>
+						<h2>{this.state.loggedInStatus}</h2>
 
-          <Switch>
-            <Route exact path = "/" component = {Home} />
+						<Switch>
+							<Route exact path="/" component={Home} />
 
-            <Route 
-              path = "/auth" 
-              render = {props => (
-                <Auth
-                  {...props}
-                  handleSuccesfulLogin = {this.handleSuccesfulLogin}
-                  handleUnsuccesfulLogin = {this.handleUnsuccesfulLogin}
-                  />
-              )}
-            />
+							<Route
+								path="/auth"
+								render={(props) => (
+									<Auth
+										{...props}
+										handleSuccesfulLogin={
+											this.handleSuccesfulLogin
+										}
+										handleUnsuccesfulLogin={
+											this.handleUnsuccesfulLogin
+										}
+									/>
+								)}
+							/>
 
-            <Route path = "/about-me" component = {About} />
-            <Route path = "/contact" component = {Contact} />
-            <Route path = "/blog" component = {Blog} />
-            <Route path = "/portfolio/:slug" component = {portfolioDetail} />
-            <Route component = {NoMatch} />
-          </Switch>
-          </div>
-        </Router>
-
-      </div>
-    );
-  }
+							<Route path="/about-me" component={About} />
+							<Route path="/contact" component={Contact} />
+							<Route path="/blog" component={Blog} />
+							<Route
+								path="/portfolio/:slug"
+								component={portfolioDetail}
+							/>
+							<Route component={NoMatch} />
+						</Switch>
+					</div>
+				</Router>
+			</div>
+		);
+	}
 }
 
 // window.alert("Today's date and time are " + moment().format('MMMM Do YYYY, h:mm:ss a'));
--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/navigation/navigation-container.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/navigation/navigation-container.js
@@ -2,34 +2,44 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavigationContainer = (props) => {
-    const dynamicLink = (route, linkText) => {
-        return (
-            <div className = "nav-link-wrapper">
-                <NavLink to = "/blog" activeClassName = "nav-link-active">Blog</NavLink>
-            </div>
-        );
-    };
-        return (
-            <div className = "nav-wrapper">
-                <div className = "left-column">
-                    <div className = "nav-link-wrapper">
-            <NavLink exact to = "/" activeClassName = "nav-link-active">Home</NavLink>
-                    </div>
+	const dynamicLink = (route, linkText) => {
+		return (
+			<div className="nav-link-wrapper">
+				<NavLink to="/blog" activeClassName="nav-link-active">
+					Blog
+				</NavLink>
+			</div>
+		);
+	};
+	return (
+		<div className="nav-wrapper">
+			<div className="left-column">
+				<div className="nav-link-wrapper">
+					<NavLink exact to="/" activeClassName="nav-link-active">
+						Home
+					</NavLink>
+				</div>
 
-                    <div className = "nav-link-wrapper">
-            <NavLink to = "/about-me" activeClassName = "nav-link-active">About</NavLink>
-                    </div>
+				<div className="nav-link-wrapper">
+					<NavLink to="/about-me" activeClassName="nav-link-active">
+						About
+					</NavLink>
+				</div>
 
-                    <div className = "nav-link-wrapper">
-            <NavLink to = "/contact" activeClassName = "nav-link-active">Contact</NavLink>
-                    </div>
+				<div className="nav-link-wrapper">
+					<NavLink to="/contact" activeClassName="nav-link-active">
+						Contact
+					</NavLink>
+				</div>
 
-                    {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
-                </div>
+				{props.loggedInStatus === "LOGGED_IN"
+					? dynamicLink("/blog", "Blog")
+					: null}
+			</div>
 
-                <div className = "right-column">Bryce Pearson</div>
-            </div>
-        )
-    }
+			<div className="right-column">Bryce Pearson</div>
+		</div>
+	);
+};
 
 export default NavigationContainer;
