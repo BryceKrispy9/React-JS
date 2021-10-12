ccf5f7407cbacbc8dca3219308b0d75d9f89d12b
BryceKrispy9 <brycepearson09@gmail.com>
2021-10-11 21:12:27 -0600

    Creating Porfolio form and calling it from Parent Component

---
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-form.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class PortfolioForm extends Component {
+	// Forms that use a state should use class component
+	render() {
+		return (
+			<div>
+				<h1>PortfolioForm</h1>
+				<h1>PortfolioForm</h1>
+			</div>
+		);
+	}
+}
