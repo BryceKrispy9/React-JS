347dd5315ee5eb42b46feaa40eae512fb6d84059
BryceKrispy9 <brycepearson09@gmail.com>
2021-10-12 20:56:12 -0600

    Adding keys to key props errors

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/app.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/app.js
@@ -80,7 +80,11 @@ export default class App extends Component {
 
 	authorizedPages() {
 		return [
-			<Route path="/portfolio-manager" component={PortfolioManager} />,
+			<Route
+				key="portfolio-manager"
+				path="/portfolio-manager"
+				component={PortfolioManager}
+			/>,
 		];
 	}
 
