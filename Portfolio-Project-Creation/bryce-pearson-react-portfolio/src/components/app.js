0a9b3f893076b54338adff7adf0d41e77e2e6200
BryceKrispy9 <brycepearson09@gmail.com>
2021-10-18 21:37:08 -0600

    Adding delete image links to the image thumbnails

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/app.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/app.js
@@ -7,6 +7,7 @@ import {
 	faTrash,
 	faSignOutAlt,
 	faEdit,
+	faMinusSquare,
 } from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -19,7 +20,7 @@ import portfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt, faEdit);
+library.add(faTrash, faSignOutAlt, faEdit, faMinusSquare);
 
 export default class App extends Component {
 	constructor(props) {
