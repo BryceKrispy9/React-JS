2998761f57ebd3b4a1c8ea0439813a43293c3a9f
BryceKrispy9 <brycepearson09@gmail.com>
2021-10-25 22:22:40 -0600

    Looking to fix small memory leak

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/blog/blog-form.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/blog/blog-form.js
@@ -47,7 +47,7 @@ export default class BlogForm extends Component {
 		event.preventDefault();
 	}
 
-	handleChange() {
+	handleChange(event) {
 		this.setState({
 			[event.target.name]: event.target.value,
 		});
