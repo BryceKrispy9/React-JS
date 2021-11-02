64321836b4905383a6dac6a43572a2e52285942f
BryceKrispy9 <brycepearson09@gmail.com>
2021-11-01 21:26:31 -0600

    Initial Build Out of the Portfolio Detail Component

--- Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-item.js
+++ Portfolio-Project-Creation/bryce-pearson-react-portfolio/src/components/portfolio/portfolio-item.js
@@ -2,45 +2,50 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class PortfolioItem extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            PortfolioItemClass: ""
-        };
-    }
+		this.state = {
+			PortfolioItemClass: "",
+		};
+	}
 
-    handleMouseEnter() {
-        this.setState({PortfolioItemClass: 'image-blur'});
-    }
+	handleMouseEnter() {
+		this.setState({ PortfolioItemClass: "image-blur" });
+	}
 
-    handleMouseLeave() {
-        this.setState({PortfolioItemClass: ""});
-    }
+	handleMouseLeave() {
+		this.setState({ PortfolioItemClass: "" });
+	}
 
-    render() {
-    const { id, description, thumb_image_url, logo_url } = this.props.item;
-    return (
-        <div className = "portfolio-item-wrapper"
-        onMouseEnter = {() => this.handleMouseEnter()}
-        onMouseLeave = {() => this.handleMouseLeave()}
-        
-        >
-        <div
-            className = {"portfolio-img-background " + this.state.PortfolioItemClass}
-            style = {{
-                backgroundImage: "url(" + thumb_image_url + ")"
-            }}
-        />
-        
-        <div className = "img-text-wrapper">
-            <div className = "logo-wrapper">
-                <img src = {logo_url} />
-            </div>
+	render() {
+		const { id, description, thumb_image_url, logo_url } = this.props.item;
+		return (
+			<Link to={`/portfolio/${id}`}>
+				<div
+					className="portfolio-item-wrapper"
+					onMouseEnter={() => this.handleMouseEnter()}
+					onMouseLeave={() => this.handleMouseLeave()}
+				>
+					<div
+						className={
+							"portfolio-img-background " +
+							this.state.PortfolioItemClass
+						}
+						style={{
+							backgroundImage: "url(" + thumb_image_url + ")",
+						}}
+					/>
 
-            <div className = "subtitle">{description}</div>
-        </div>
-        </div>
-        );
-    }
+					<div className="img-text-wrapper">
+						<div className="logo-wrapper">
+							<img src={logo_url} />
+						</div>
+
+						<div className="subtitle">{description}</div>
+					</div>
+				</div>
+			</Link>
+		);
+	}
 }
